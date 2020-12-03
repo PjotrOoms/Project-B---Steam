0385cb716f031aef06e4272b4afc75eb7c38dc96
PjotrOoms <75120770+PjotrOoms@users.noreply.github.com>
2020-12-03 23:53:35 +0100

    Update Sprint 1.py

--- Sprint 1.py
+++ Sprint 1.py
@@ -1,15 +1,39 @@
+#  ---Imports-----------------------------------------------------------------------------------------------------------
+from tkinter import *
 import json
 from operator import itemgetter
 
 with open('steam.json', 'r') as steamfile:
     data = json.load(steamfile)
 
+    def naam1():
+        game_name = (data[0]['name'])
+        text_box.delete("1.0", "end")
+        text_box.insert("end-1c", f'{game_name}')
+
+
     def sort():
         sortkey = input('sortkey')
-        data.sort(key=itemgetter(sortkey))
-        count = 0
-        for game in data:
-            print(game)
-            count += 1
-        print(count)
-    sort()
+        sorted(data, key=itemgetter(sortkey))
+
+
+#  ---Tkinter-GUI-------------------------------------------------------------------------------------------------------
+
+root_steam = Tk()
+root_steam.geometry("600x400")
+root_steam.title("STEAM")
+
+#  Label spel
+mod_label = Label(root_steam, text="Naam spel:")
+mod_label.place(x=30, y=20)
+
+#  Textvak spel
+text_box = Text(root_steam, width=60, height=12)
+text_box.place(x=100, y=20)
+
+#  Toon naam spel buttton
+ber_btn = Button(root_steam, text="Toon spel",  command=naam1)
+ber_btn.place(x=20, y=300, width=90)
+
+
+root_steam.mainloop()
